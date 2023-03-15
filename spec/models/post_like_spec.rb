02525dd94c7d71e82b744ec5f6337f4cf821e700
adrian-y1 <adrian.yonan1@gmail.com>
2023-03-15 14:37:14 +1100

    Created: Association & Validation tests for PostLike model

--- spec/models/post_like_spec.rb
+++ spec/models/post_like_spec.rb
@@ -15,5 +15,40 @@
 require 'rails_helper'
 
 RSpec.describe PostLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    describe "Liker" do
+      it "is valid when the liker & liked post is unique" do
+        user = create(:user)
+        post = create(:post)
+        post_like = create(:post_like, liker: user, liked_post: post)
+        expect(post_like).to be_valid
+      end
+
+      it "is not valid when the liker & liked post is not unique" do
+        user = create(:user)
+        post = create(:post)
+        post_like1 = create(:post_like, liker: user, liked_post: post)
+        post_like2 = build(:post_like, liker: user, liked_post: post)
+        expect(post_like2).to_not be_valid
+      end
+    end
+  end
+
+  describe "Associations" do
+    describe "User" do
+      it "belongs to a user(liker)" do
+        user = create(:user)
+        post_like = create(:post_like, liker: user)
+        expect(post_like.liker).to eq(user)
+      end      
+    end
+
+    describe "Post" do
+      it "belongs to a post(liked_post)" do
+        post = create(:post)
+        post_like = create(:post_like, liked_post: post)
+        expect(post_like.liked_post).to eq(post)
+      end
+    end
+  end
 end

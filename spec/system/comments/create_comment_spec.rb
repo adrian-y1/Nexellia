a12160a55e02bf00dc99610ba73206023cd284b4
adrian-y1 <adrian.yonan1@gmail.com>
2023-03-23 14:35:54 +1100

    Removed old system tests for CommentsController's Create action and added new ones.
    Previous system tests for creating a comment did not properly test Turbo Streams and Turbo Frames functionality.
    
    The reason was because i was using Capybara's RackTest drive instead of the Selenium drive.
    Since Turbo Streams runs on JavaScript and RackTest is not aware of JavaScript, the Selenium driver
    must be used to properly test my application as it is a "browser automation tool".
    
    This commit removes old tests and adds new tests that use the #post_[post.id] Turbo frame tag
    to properly test that a comment is created live without a full page refresh.
    
    With these new tests in place, they can now properly check that Turbo Streams works by checking that
    the comment is created live without a page refresh, and that the flash notice is displayed for both sucess
    and error cases.
    
    Overall, these new tests provide more thorough testing of the create comment functionality.

--- spec/system/comments/create_comment_spec.rb
+++ spec/system/comments/create_comment_spec.rb
@@ -1,44 +1,140 @@
 require 'rails_helper'
 
-RSpec.describe "Create Comment", type: :system do
+RSpec.describe "Create Comment", type: :system, js: true do
   let(:user) { create(:user) }
+  let(:content) { "I just commented!" }
+  let(:flash_notice) { 'Comment was successfully created.' }
   
   before do
-    driven_by(:rack_test)
     login_as(user)
+    driven_by(:selenium_chrome_headless)
   end
 
-  # Im rendering the create comment form under the post, and the post is rendered in posts_path (index page) 
-  # the comment form is under the respective post and since im using Turbo Streams,
-  # i dont need to visit/redirect to posts_path since im already there otherwise i wont see the flash notice
-  describe "Valid Attributes" do
-    it "creates the comment with Turbo Streams" do
-      post = create(:post, user: user)
-      content = "I just commented!"
-      flash_notice = 'Comment was successfully created.'
-      visit new_post_comment_path(post)
-      fill_in 'comment[body]', with: content
-      click_on 'Create Comment'
+  describe "posts#index Page" do
+    # These tests check comment creation inside the posts#index page with valid and 
+    # invalid attributes. And they also test to see if Turbo Streams is working.
 
-      # Since the user hasn't been redirected after creating a comment on a post,
-      # this confirms that they are still on the index page and Turbo Streams is working.
-      expect(page).to have_content(content)
-      expect(page).to have_content(flash_notice)
-      expect(page.current_url).to eq(posts_url)
+    describe "Valid Attributes" do
+      # This test checks that a comment can be successfully created using the create comment form
+      # that is displayed inside a post. With the help of Turbo Frames, i don't need to visit a seperate
+      # page to access the form as it can be done so in the posts#index page where the posts are listed.
+      #
+      # After the form is filled and submitted, the comment is created live and the user 
+      # is not redirected to a new page as a result of using Turbo Streams.
+      # 
+      # The expect statements ensure that the comment is successfully created and that a flash notice is
+      # displayed to the user. They also confirm that Turbo Streams is working by checking
+      # that the comment is created live without a page refresh.
+
+      it "creates the comment with Turbo Streams" do
+        post = create(:post, user: user)
+        visit posts_path
+
+        post_frame = find("turbo-frame#post_#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: content
+          click_on 'Create Comment'
+        end
+
+        # Confirms that the comment was created live using Turbo Streams and that 
+        # the user is still on the posts#index page.
+        expect(page).to have_current_path(posts_path)
+        expect(page).to have_content(content)
+        expect(page).to have_content(flash_notice)
+      end
+    end
+
+    describe "Invalid Attributes" do
+      # Similar to the 'Valid Attributes' test, however this test covers the the case when
+      # user attempts to submit an empty body. Again, with the help of Turbo Frames, i don't need to visit a seperate
+      # page to access the form as it can be done so in the posts#index page where the posts are listed.
+      #
+      # After the form is submitted with empty body, the flash notice is displayed to the user
+      # with no page refresh/reload.
+      # 
+      # The expect statements ensure that the comment was not created and that a flash notice is
+      # displayed to the user. They also confirm that Turbo Streams is working by checking
+      # that the comment is created live without a page refresh.
+
+      it "doesn't create the comment and renders an error with Turbo Streams" do
+        post = create(:post, user: user)
+        visit posts_path
+
+        post_frame = find("turbo-frame#post_#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: ''
+          click_on 'Create Comment'
+        end
+
+        # Since the user hasn't been redirected after encountering an error with creating a comment,
+        # this confirms Turbo Streams is working and that they are on the posts#index page.
+        expect(page).to have_current_path(posts_path)
+        expect(page).to have_content("Body can't be blank")
+      end
     end
   end
 
-  describe "invalid Attributes" do
-    it "doesn't create the comment and renders an error with Turbo Streams" do
-      post = create(:post, user: user)
-      visit new_post_comment_path(post)
-      fill_in 'comment[body]', with: ''
-      click_on 'Create Comment'
+  describe "posts#show Page" do
+    # These tests check comment creation inside the posts#show page with valid and 
+    # invalid attributes. And they also test to see if Turbo Streams is working.
 
-      # Since the user hasn't been redirected after encountering an error with creating a comment,
-      # this confirms Turbo Streams is working and that they are on the comments#index page.
-      expect(page).to have_content("Body can't be blank")
-      expect(page).to have_current_path(post_comments_path(post))
+    describe "Valid Attributes" do
+      # This test checks that a comment can be successfully created using the create comment form
+      # that is displayed inside a post on the posts#show page. With the help of Turbo Frames, i don't need to visit a seperate
+      # page to access the form as it can be done so on the show page of the desired post.
+      #
+      # After the form is filled and submitted, the comment is created live and the user 
+      # is not redirected to a new page as a result of using Turbo Streams.
+      # 
+      # The expect statements ensure that the comment is successfully created and that a flash notice is
+      # displayed to the user. They also confirm that Turbo Streams is working by checking
+      # that the comment is created live without a page refresh.
+
+      it "creates the comment with Turbo Streams" do
+        post = create(:post, user: user)
+        visit post_path(post)
+
+        post_frame = find("turbo-frame#post_#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: content
+          click_on 'Create Comment'
+        end
+
+        # Confirms that the comment was created live using Turbo Streams and that 
+        # the user is still on the posts#show page.
+        expect(page).to have_current_path(post_path(post))
+        expect(page).to have_content(content)
+        expect(page).to have_content(flash_notice)
+      end
+    end
+
+    describe "Invalid Attributes" do
+      # Similar to the 'Valid Attributes' test, however this test covers the the case when
+      # user attempts to submit an empty body. Again, with the help of Turbo Frames, i don't need to visit a seperate
+      # page to access the form as it can be done so on the show page of the post.
+      #
+      # After the form is submitted with empty body, the flash notice is displayed to the user
+      # with no page refresh/reload.
+      # 
+      # The expect statements ensure that the comment was not created and that a flash notice is
+      # displayed to the user. They also confirm that Turbo Streams is working by checking
+      # that the comment is created live without a page refresh.
+
+      it "doesn't create the comment and renders an error with Turbo Streams" do
+        post = create(:post, user: user)
+        visit post_path(post)
+
+        post_frame = find("turbo-frame#post_#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: ''
+          click_on 'Create Comment'
+        end
+
+        # Since the user hasn't been redirected after encountering an error with creating a comment,
+        # this confirms Turbo Streams is working and that they are on the posts#show page.
+        expect(page).to have_current_path(post_path(post))
+        expect(page).to have_content("Body can't be blank")
+      end
     end
   end
 end

b96e161cf8dfc2001d9eb79208cd4c9cea0ecf6b
adrian-y1 <adrian.yonan1@gmail.com>
2023-03-31 16:41:22 +1100

    Modified previous code for cancelling a friend request to implement declining a friend request feature.
    In this commit, i modified the previous code for cancelling a friend request in order to make it more flexible
    so that it can also handle declining a friend request.
    
    However, there are still ongoing issues due to the usage of HTTP rather than websockets. One of the issues is that
    when the user sends a friend request to another user and they cancel it, if the receiver has not refreshed the page after receiving
    the request, they will still see Accept or Decline forms. If the user clicks on Decline, an error message is generated.
    
    The previous issue with Cancel & Accept is still occuring as well. To fix these, i may need to implement Turbo Streams.

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -11,12 +11,14 @@ class FriendRequestsController < ApplicationController
   end
 
   def destroy
-    @friend_request = current_user.friend_requests_sent.find_by(receiver_id: params[:receiver_id])
+    @friend_request = FriendRequest.find_by(sender_id: params[:sender_id], receiver_id: params[:receiver_id])
     
     if @friend_request
       @receiver_username = @friend_request.receiver.username
+      @sender_username = @friend_request.sender.username
       @friend_request.destroy
-      redirect_to request.referrer, notice: "Friend request to #{@receiver_username} has been cancelled."
+
+      redirect_to request.referrer, notice: destroy_alert_message(@sender_username, @receiver_username)
     else
       redirect_to request.referrer, status: :unprocessable_entity, alert: "Error while cancelling friend request."
     end
@@ -24,8 +26,14 @@ class FriendRequestsController < ApplicationController
 
   private
 
+  def destroy_alert_message(sender_username, receiver_username)
+    receiver_msg = "You have declined #{sender_username}'s friend request."
+    sender_msg = "Friend request to #{receiver_username} has been cancelled."
+    sender_username == current_user.username ? sender_msg : receiver_msg
+  end
+
   def friend_request_params
-    params.require(:friend_request).permit(:receiver_id)
+    params.require(:friend_request).permit(:sender_id, :receiver_id)
   end
   
 end

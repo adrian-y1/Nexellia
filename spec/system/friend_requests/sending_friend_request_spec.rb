5cce3c7f06327e2f9363bbacf125e0f048919692
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-06 16:39:49 +1000

    Modified system test for sending a friend request on the users#index page to account for Turbo Streams and Turbo Frames.
    This commit modifies the previous system test for sending a friend request on the users#index page to account for the new functionality
    whereby friend requests are sent in real-time with Turbo Streams.
    
    Upon creating this system test, i discovered that the test was "flaky", meaning it was sometimes passing and sometimes failing
    without any code change. The cause of this issue was that the broadcasts were happening before turbo_stream_from was able
    to establish a websocket connection.
    
    To fix this, i added an expectation statement, before the broadcast call happens, for `turbo-cable-stream-source[connected]` to ensure that the subscription to
    the stream was connected.

--- spec/system/friend_requests/sending_friend_request_spec.rb
+++ spec/system/friend_requests/sending_friend_request_spec.rb
@@ -1,32 +1,49 @@
-# This file contains system tests for sending friend requests. 
+# This file contains system tests for sending friend requests using Turbo Streams. 
 #
-# These tests are created using Rspec + Capybara + Capybara's rack_test drive
+# These tests are created using Rspec + Capybara + Capybara's selenium drive
 #
 # To run the tests, run => rspec spec/system/friend_requests/sending_friend_request_spec.rb
 
 require "rails_helper"
 
-RSpec.describe "Send Friend Request", type: :system do 
+RSpec.describe "Send Friend Request", type: :system , js: true do 
   let(:user) { create(:user) }
 
   before do
     login_as(user)
-    driven_by(:rack_test)
+    driven_by(:selenium_chrome_headless)
   end
 
   describe "users#index page" do
     # This test checks the functionality for sending a friend request on the
-    # users#index page.
+    # users#index using Turbo Streams.
     
     context "when clicking the add friend button on the users#index page" do
-      it "sends a friend request, renders a successfull flash notice message and redirects to the users#index page" do
+      # First, ensure that the subscription to the stream has been connected to avoid flaky tests
+      # whereby Turbo Stream broadcasts happen before turbo_stream_from can establish a websocket connection.
+      #
+      # Then, send the friend request by clicking the 'Add Friend' button after finding the Turbo Frame
+      # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
+      #
+      # Lastly, the expectations for the flash notice, 'Cancel' button and the current path, ensures
+      # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
+      # Meaning Turbo Streams is working.
+
+      it "successfully sends a friend request on users#index  using Turbo Streams" do
         new_user = create(:user)
         visit users_path
-  
-        click_on "Add Friend"
-  
-        # The flash notice and the 'Cancel' button ensure that the Friend Request object was
-        # created, meaning the request was sent to the user
+
+        # To avoid flaky tests, this expectation must be asserted to check that the subscription
+        # has been connected to the stream before making a broadcast call.
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+
+        receiver_frame = find("turbo-frame#user_#{new_user.id}")
+        within(receiver_frame) do
+          click_on "Add Friend"
+        end
+
+        # The flash notice, the 'Cancel' button and the current path, ensure that the Friend Request object was
+        # created, meaning the request was sent to the user in real-time using Turbo Streams.
         expect(page).to have_content("Friend request has been sent to #{new_user.username}!")
         expect(page).to have_current_path(users_path)
         expect(page).to have_button("Cancel")

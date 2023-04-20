4cd107702baf06d1b2750a8290a51a7f2deba95e
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-20 15:26:11 +1000

    Implement system tests for sending and declining a friend request in the users#show page using Turbo Streams

--- spec/system/friend_requests/declining_friend_request_spec.rb
+++ spec/system/friend_requests/declining_friend_request_spec.rb
@@ -14,20 +14,20 @@ RSpec.describe "Decline Friend Request", type: :system, js: true do
     driven_by(:selenium_chrome_headless)
   end
 
+  # First, ensure that the subscription to the stream has been connected before the broadcast gets called to avoid flaky tests
+  #
+  # Then, decline the friend request by clicking the 'Decline' button after finding the Turbo Frame
+  # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
+  #
+  # Lastly, the expectations for the flash notice, 'Add Friend' button and the current path, ensures
+  # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
+  # Meaning Turbo Streams is working.
+
   describe "users#index page" do
     # This test checks the functionality for declining a friend request on the
     # users#index page using Turbo Streams.
     
     context "when clicking the decline button on the users#index page" do
-      # First, ensure that the subscription to the stream has been connected before the broadcast gets called to avoid flaky tests
-      #
-      # Then, decline the friend request by clicking the 'Decline' button after finding the Turbo Frame
-      # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
-      #
-      # Lastly, the expectations for the flash notice, 'Add Friend' button and the current path, ensures
-      # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
-      # Meaning Turbo Streams is working.
-
       it "successfully declines the friend request on the users#index page using Turbo Streams" do
         new_user = create(:user)
         create(:friend_request, sender: new_user, receiver: user)
@@ -42,6 +42,30 @@ RSpec.describe "Decline Friend Request", type: :system, js: true do
         
         expect(page).to have_content("You have declined #{new_user.username}'s friend request.")
         expect(page).to have_current_path(users_path)
+        expect(page).to have_button("Add Friend")
+      end
+    end
+  end
+
+  describe "users#show page" do
+    # This test checks the functionality for declining a friend request on the
+    # users#show page using Turbo Streams.
+    
+    context "when clicking the decline button on the users#show page" do
+      it "successfully declines the friend request on the users#show page using Turbo Streams" do
+        new_user = create(:user)
+        create(:friend_request, sender: new_user, receiver: user)
+        visit user_path(new_user)
+        
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+
+        receiver_frame = find("turbo-frame#user_#{new_user.id}")
+        within(receiver_frame) do
+          click_on "Decline"
+        end
+        
+        expect(page).to have_content("You have declined #{new_user.username}'s friend request.")
+        expect(page).to have_current_path(user_path(new_user))
         expect(page).to have_button("Add Friend")
       end
     end
--- spec/system/friend_requests/sending_friend_request_spec.rb
+++ spec/system/friend_requests/sending_friend_request_spec.rb
@@ -14,21 +14,21 @@ RSpec.describe "Send Friend Request", type: :system , js: true do
     driven_by(:selenium_chrome_headless)
   end
 
+  # First, ensure that the subscription to the stream has been connected to avoid flaky tests
+  # whereby Turbo Stream broadcasts happen before turbo_stream_from can establish a websocket connection.
+  #
+  # Then, send the friend request by clicking the 'Add Friend' button after finding the Turbo Frame
+  # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
+  #
+  # Lastly, the expectations for the flash notice, 'Cancel' button and the current path, ensures
+  # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
+  # Meaning Turbo Streams is working.
+
   describe "users#index page" do
     # This test checks the functionality for sending a friend request on the
     # users#index using Turbo Streams.
     
     context "when clicking the add friend button on the users#index page" do
-      # First, ensure that the subscription to the stream has been connected to avoid flaky tests
-      # whereby Turbo Stream broadcasts happen before turbo_stream_from can establish a websocket connection.
-      #
-      # Then, send the friend request by clicking the 'Add Friend' button after finding the Turbo Frame
-      # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
-      #
-      # Lastly, the expectations for the flash notice, 'Cancel' button and the current path, ensures
-      # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
-      # Meaning Turbo Streams is working.
-
       it "successfully sends a friend request on users#index  using Turbo Streams" do
         new_user = create(:user)
         visit users_path
@@ -46,6 +46,33 @@ RSpec.describe "Send Friend Request", type: :system , js: true do
         # created, meaning the request was sent to the user in real-time using Turbo Streams.
         expect(page).to have_content("Friend request has been sent to #{new_user.username}!")
         expect(page).to have_current_path(users_path)
+        expect(page).to have_button("Cancel")
+      end
+    end
+  end
+
+  describe "users#show page" do
+    # This test checks the functionality for sending a friend request on the
+    # users#show using Turbo Streams.
+    
+    context "when clicking the add friend button on the users#show page" do
+      it "successfully sends a friend request on users#show  using Turbo Streams" do
+        new_user = create(:user)
+        visit user_path(new_user)
+
+        # To avoid flaky tests, this expectation must be asserted to check that the subscription
+        # has been connected to the stream before making a broadcast call.
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+
+        receiver_frame = find("turbo-frame#user_#{new_user.id}")
+        within(receiver_frame) do
+          click_on "Add Friend"
+        end
+
+        # The flash notice, the 'Cancel' button and the current path, ensure that the Friend Request object was
+        # created, meaning the request was sent to the user in real-time using Turbo Streams.
+        expect(page).to have_content("Friend request has been sent to #{new_user.username}!")
+        expect(page).to have_current_path(user_path(new_user))
         expect(page).to have_button("Cancel")
       end
     end

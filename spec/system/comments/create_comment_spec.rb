16213be5ab9e59a0b096fc916c3c66a1046909d6
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-21 18:08:42 +1000

    Added system tests for the Create and Delete comment functionality for the users#show page

--- spec/system/comments/create_comment_spec.rb
+++ spec/system/comments/create_comment_spec.rb
@@ -16,21 +16,22 @@ RSpec.describe "Create Comment", type: :system, js: true do
     driven_by(:selenium_chrome_headless)
   end
 
-  describe "posts#index Page" do
-    # These tests check comment creation inside the posts#index page with valid and 
-    # invalid attributes. And they also test to see if Turbo Streams is working.
+  describe "Valid Attributes" do
+    # These tests check that a comment can be successfully created with valid attributes using 
+    # the create comment form that is displayed inside a post. 
+    #
+    # With the help of Turbo Frames, i don't need to visit a seperate 
+    # page to access the form.
+    #
+    # After the form is filled with data and submitted, the comment is created live and the user
+    # is not redirect to a new page as a result of using Turbo Streams.
+    #
+    # The expect statements ensure that the comment is successfully created and that a flash notice 
+    # is displayed to the user. They also confirm that Turbo Streams is working by checking the comment
+    # is created and broadcasted live without a page refresh.
 
-    describe "Valid Attributes" do
-      # This test checks that a comment can be successfully created using the create comment form
-      # that is displayed inside a post. With the help of Turbo Frames, i don't need to visit a seperate
-      # page to access the form as it can be done so in the posts#index page where the posts are listed.
-      #
-      # After the form is filled and submitted, the comment is created live and the user 
-      # is not redirected to a new page as a result of using Turbo Streams.
-      # 
-      # The expect statements ensure that the comment is successfully created and that a flash notice is
-      # displayed to the user. They also confirm that Turbo Streams is working by checking
-      # that the comment is created live without a page refresh.
+    describe "posts#index page" do
+      # This test checks comment creation with valid attributes in the posts#index page using Turbo Streams.
 
       it "creates the comment with Turbo Streams" do
         post = create(:post, user: user)
@@ -50,51 +51,8 @@ RSpec.describe "Create Comment", type: :system, js: true do
       end
     end
 
-    describe "Invalid Attributes" do
-      # Similar to the 'Valid Attributes' test, however this test covers the the case when
-      # user attempts to submit an empty body. Again, with the help of Turbo Frames, i don't need to visit a seperate
-      # page to access the form as it can be done so in the posts#index page where the posts are listed.
-      #
-      # After the form is submitted with empty body, the flash notice is displayed to the user
-      # with no page refresh/reload.
-      # 
-      # The expect statements ensure that the comment was not created and that a flash notice is
-      # displayed to the user. They also confirm that Turbo Streams is working by checking
-      # that the comment is created live without a page refresh.
-
-      it "doesn't create the comment and renders an error with Turbo Streams" do
-        post = create(:post, user: user)
-        visit posts_path
-
-        post_frame = find("turbo-frame#post-interactions-#{post.id}")
-        within(post_frame) do
-          fill_in 'comment[body]', with: ''
-          click_on 'Create Comment'
-        end
-
-        # Since the user hasn't been redirected after encountering an error with creating a comment,
-        # this confirms Turbo Streams is working and that they are on the posts#index page.
-        expect(page).to have_current_path(posts_path)
-        expect(page).to have_content("Body can't be blank")
-      end
-    end
-  end
-
-  describe "posts#show Page" do
-    # These tests check comment creation inside the posts#show page with valid and 
-    # invalid attributes. And they also test to see if Turbo Streams is working.
-
-    describe "Valid Attributes" do
-      # This test checks that a comment can be successfully created using the create comment form
-      # that is displayed inside a post on the posts#show page. With the help of Turbo Frames, i don't need to visit a seperate
-      # page to access the form as it can be done so on the show page of the desired post.
-      #
-      # After the form is filled and submitted, the comment is created live and the user 
-      # is not redirected to a new page as a result of using Turbo Streams.
-      # 
-      # The expect statements ensure that the comment is successfully created and that a flash notice is
-      # displayed to the user. They also confirm that Turbo Streams is working by checking
-      # that the comment is created live without a page refresh.
+    describe "posts#show page" do
+      # This test checks comment creation with valid attributes in the posts#show page using Turbo Streams.
 
       it "creates the comment with Turbo Streams" do
         post = create(:post, user: user)
@@ -114,18 +72,59 @@ RSpec.describe "Create Comment", type: :system, js: true do
       end
     end
 
-    describe "Invalid Attributes" do
-      # Similar to the 'Valid Attributes' test, however this test covers the the case when
-      # user attempts to submit an empty body. Again, with the help of Turbo Frames, i don't need to visit a seperate
-      # page to access the form as it can be done so on the show page of the post.
-      #
-      # After the form is submitted with empty body, the flash notice is displayed to the user
-      # with no page refresh/reload.
-      # 
-      # The expect statements ensure that the comment was not created and that a flash notice is
-      # displayed to the user. They also confirm that Turbo Streams is working by checking
-      # that the comment is created live without a page refresh.
+    describe "users#show page" do
+      # This test checks comment creation with valid attributes in the users#show page using Turbo Streams.
 
+      it "creates the comment with Turbo Streams" do
+        post = create(:post, user: user)
+        visit user_path(post.user)
+
+        post_frame = find("turbo-frame#post-interactions-#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: content
+          click_on 'Create Comment'
+        end
+
+        # Confirms that the comment was created live using Turbo Streams and that 
+        # the user is still on the users#show page.
+        expect(page).to have_current_path(user_path(post.user))
+        expect(page).to have_content(content)
+        expect(page).to have_content(flash_notice)
+      end
+    end
+  end
+
+  describe "Invalid Attributes" do
+    # These tests check that a comment cannot be created with an empty body
+    #
+    # With the help of Turbo Frames, i don't need to visit a seperate page to access the create comment form.
+    #
+    # After the form is filled with invalid data and submitted, the error message is displayed to the user
+    # without a page refresh as a result of using Turbo Streams and Turbo Frames.
+    #
+    # The expect statements ensure that the comment was not created and that a flash notice is
+    # displayed to the user. They also confirm that Turbo Streams is working by checking
+    # that the comment is not created live without a page refresh.
+
+    describe "posts#index page" do
+      it "doesn't create the comment and renders an error with Turbo Streams" do
+        post = create(:post, user: user)
+        visit posts_path
+
+        post_frame = find("turbo-frame#post-interactions-#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: ''
+          click_on 'Create Comment'
+        end
+
+        # Since the user hasn't been redirected after encountering an error with creating a comment,
+        # this confirms Turbo Streams is working and that they are on the posts#index page.
+        expect(page).to have_current_path(posts_path)
+        expect(page).to have_content("Body can't be blank")
+      end
+    end
+
+    describe "posts#show page" do
       it "doesn't create the comment and renders an error with Turbo Streams" do
         post = create(:post, user: user)
         visit post_path(post)
@@ -139,6 +138,24 @@ RSpec.describe "Create Comment", type: :system, js: true do
         # Since the user hasn't been redirected after encountering an error with creating a comment,
         # this confirms Turbo Streams is working and that they are on the posts#show page.
         expect(page).to have_current_path(post_path(post))
+        expect(page).to have_content("Body can't be blank")
+      end
+    end
+
+    describe "users#show page" do
+      it "doesn't create the comment and renders an error with Turbo Streams" do
+        post = create(:post, user: user)
+        visit user_path(post.user)
+
+        post_frame = find("turbo-frame#post-interactions-#{post.id}")
+        within(post_frame) do
+          fill_in 'comment[body]', with: ''
+          click_on 'Create Comment'
+        end
+
+        # Since the user hasn't been redirected after encountering an error with creating a comment,
+        # this confirms Turbo Streams is working and that they are on the posts#show page.
+        expect(page).to have_current_path(user_path(post.user))
         expect(page).to have_content("Body can't be blank")
       end
     end
--- spec/system/comments/delete_comment_spec.rb
+++ spec/system/comments/delete_comment_spec.rb
@@ -15,17 +15,17 @@ RSpec.describe "Delete Comment", type: :system, js: true do
     driven_by(:selenium_chrome_headless)
   end
 
-  describe "posts#index Page" do
-    # This test checks comment deletion inside the posts#index page
-    # and they also test to see if Turbo Streams is working.
-    #
-    # With the help of Turbo Frames, i don't need to visit a seperate page to 
-    # access the delete button as it can be done so in the posts#index page where the posts are listed.
-    # 
-    # The expect statements ensure that the comment is successfully deleted and that a flash notice is
-    # displayed to the user. They also confirm that Turbo Streams is working by checking
-    # that the comment is deleted live without a page refresh.
+  # These tests check comment deletion inside using Turbo Streams.
+  #
+  # With the help of Turbo Frames, i don't need to visit a seperate page to 
+  # access the delete button as it is displayed on the comment itself.
+  # 
+  # The expect statements ensure that the comment is successfully deleted and that a flash notice is
+  # displayed to the user. They also confirm that Turbo Streams is working by checking
+  # that the comment is deleted live without a page refresh.
 
+    
+  describe "posts#index Page" do
     before do
       post = create(:post, user: user)
       comment = create(:comment, post: post, user: user)
@@ -38,25 +38,12 @@ RSpec.describe "Delete Comment", type: :system, js: true do
         click_on "Delete"
       end
      
-      
-      # Confirms that the comment was deleted live using Turbo Streams and that 
-      # the user is still on the posts#index page.
       expect(page).to have_current_path(posts_path)
       expect(page).to have_content(flash_notice)
     end
   end
 
   describe "posts#show Page" do
-    # This test checks comment deletion inside the posts#show page with valid
-    # and they also test to see if Turbo Streams is working.
-    #
-    # With the help of Turbo Frames, i don't need to visit a seperate page to 
-    # access the delete button as it can be done so in the posts#show page.
-    # 
-    # The expect statements ensure that the comment is successfully deleted and that a flash notice is
-    # displayed to the user. They also confirm that Turbo Streams is working by checking
-    # that the comment is deleted live without a page refresh.
-
     before do
       post = create(:post, user: user)
       comment = create(:comment, post: post, user: user)
@@ -74,6 +61,26 @@ RSpec.describe "Delete Comment", type: :system, js: true do
       # Confirms that the comment was deleted live using Turbo Streams and that 
       # the user is still on the posts#show page.
       expect(page).to have_current_path(post_path(post))
+      expect(page).to have_content(flash_notice)
+    end
+  end
+
+  describe "users#show Page" do
+    before do
+      post = create(:post, user: user)
+      comment = create(:comment, post: post, user: user)
+    end
+
+    it "deletes the comment and renders flash notice using Turbo Streams" do
+      post = user.posts.last
+      visit user_path(post.user)
+
+      comment_frame = find("turbo-frame#comment_#{post.comments.last.id}")
+      within(comment_frame) do
+        click_on "Delete"
+      end
+
+      expect(page).to have_current_path(user_path(post.user))
       expect(page).to have_content(flash_notice)
     end
   end

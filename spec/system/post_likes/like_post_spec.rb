99b0393fa0a8fb870be073f9de7a240d39dd14f2
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-21 15:48:21 +1000

    Implement system tests for the Post Like/Unlike functionality in users#show page using Turbo Streams

--- spec/system/post_likes/like_post_spec.rb
+++ spec/system/post_likes/like_post_spec.rb
@@ -14,19 +14,19 @@ RSpec.describe "Like Post", type: :system, js: true do
     driven_by(:selenium_chrome_headless)
   end
 
+  # First, ensure that the subscription to the stream has been connected before the broadcast gets called to avoid flaky tests
+  #
+  # Then, like the post by clicking the 'Like' button after finding the Turbo Frame 
+  # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
+  #
+  # Lastly, the expectations for the 'Unlike' button, '1 Like' (counter incremented) and the current path, ensures
+  # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
+  # Meaning Turbo Streams is working.
+
   describe "posts#index page" do
     # This test checks the functionality of liking posts on the posts#index page using Turbo Streams. 
-    # It ensures that the user gets instant feedback upon liking a post, such as incrementing the 
-    # likes counter and displaying the unlike button. 
-    #
-    # The test verifies the rendering of the like button and likes counter, and then clicks on 
-    # the like button. 
-    #
-    # The subsequent expectation statements ensure that the user is still on the posts#index page, 
-    # the 'Unlike' button has appeared, the Turbo Frame for likes has been rendered, and the likes counter has incremented. 
-    # These checks confirm that Turbo Stream is working without the need for a page refresh.
 
-    context "when liking a post on the index page" do
+    context "when liking a post on the posts#index page" do
       it "increments the likes counter and displays unlike button live using Turbo Streams" do
         post = create(:post, user: user)
         visit posts_path
@@ -51,17 +51,8 @@ RSpec.describe "Like Post", type: :system, js: true do
 
   describe "posts#show page" do
     # This test checks the functionality of liking posts on the posts#show page using Turbo Streams. 
-    # It ensures that the user gets instant feedback upon liking a post, such as incrementing the 
-    # likes counter and displaying the unlike button. 
-    #
-    # The test verifies the rendering of the like button and likes counter, and then clicks on 
-    # the like button. 
-    #
-    # The subsequent expectation statements ensure that the user is still on the posts#show page, 
-    # the 'Unlike' button has appeared, the Turbo Frame for likes has been rendered, and the likes counter has incremented. 
-    # These checks confirm that Turbo Stream is working without the need for a page refresh.
 
-    context "when liking a post on the show page" do
+    context "when liking a post on the posts#show page" do
       it "increments the likes counter and displays unlike button live using Turbo Streams" do
         post = create(:post, user: user)
         visit post_path(post)
@@ -77,6 +68,32 @@ RSpec.describe "Like Post", type: :system, js: true do
         end
         
         expect(page).to have_current_path(post_path(post))
+        expect(page).to have_button('Unlike')
+        expect(page).to have_selector("turbo-frame#post_#{post.id}_likes")
+        expect(page).to have_content('1 Like')
+      end
+    end
+  end
+
+  describe "users#show page" do
+    # This test checks the functionality of liking posts on the users#show page using Turbo Streams. 
+
+    context "when liking a post on the users#show page" do
+      it "increments the likes counter and displays unlike button live using Turbo Streams" do
+        post = create(:post, user: user)
+        visit user_path(post.user)
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+
+        expect(page).to have_button('Like')
+        expect(page).to have_content('0 Likes')
+
+        private_likes_frame = find("turbo-frame#post_#{post.id}\\ private_likes")
+        within(private_likes_frame) do
+          click_button 'Like'
+        end
+        
+        expect(page).to have_current_path(user_path(post.user))
         expect(page).to have_button('Unlike')
         expect(page).to have_selector("turbo-frame#post_#{post.id}_likes")
         expect(page).to have_content('1 Like')
--- spec/system/post_likes/unlike_post_spec.rb
+++ spec/system/post_likes/unlike_post_spec.rb
@@ -14,20 +14,17 @@ RSpec.describe 'Unlike Post', type: :system, js: true do
     driven_by(:selenium_chrome_headless)
   end
 
+  # First, ensure that the subscription to the stream has been connected before the broadcast gets called to avoid flaky tests
+  #
+  # Then, unlike the post by clicking the 'Unlike' button after finding the Turbo Frame 
+  # that is wrapped around it. Due to having a Turbo Frame tag, a visit to another page is not needed.
+  #
+  # Lastly, the expectations for the 'Like' button, '0 Likes' (counter decremented) and the current path, ensures
+  # that the user is receiving updates as they happen in real-time, without a page refresh/reload.
+  # Meaning Turbo Streams is working.
+
   describe "posts#index page" do
     # This test checks unliking posts on the posts#index page using Turbo Streams.
-    # With the use of Turbo Streams, the user is able to get instant feedback upon unliking a post.
-    # This includes decrementing the likes counter and displaying the like button instead.
-    #
-    # The first couple expectation statements are used to make sure that the 
-    # unlike button has been rendered into the page as well as the likes counter.
-    #
-    # After that, the test clicks on "Unlike" button after finding it's Turbo Frame.
-    # 
-    # The expectation statements following it ensures that the user is still on the posts#index page,
-    # the 'Like' button has appeared, the Turbo Frame for likes has been rendered,
-    # and the likes counter has decremented. 
-    # These all ensure that Turbo Stream is working because there hasn't been a page refresh.
 
     context "when unliking a post on the posts#index page" do
       it "decrements the likes counter and displays like button live using Turbo Streams" do
@@ -55,15 +52,6 @@ RSpec.describe 'Unlike Post', type: :system, js: true do
 
   describe "posts#show page" do
     # This test checks the functionality of unliking posts on the posts#show page using Turbo Streams. 
-    # It ensures that the user gets instant feedback upon unliking a post, such as decrementing the 
-    # likes counter and displaying the like button. 
-    #
-    # The test verifies the rendering of the unlike button and likes counter, and then clicks on 
-    # the unlike button. 
-    #
-    # The subsequent expectation statements ensure that the user is still on the posts#show page, 
-    # the 'Like' button has appeared, the Turbo Frame for likes has been rendered, and the likes counter has decremented. 
-    # These checks confirm that Turbo Stream is working without the need for a page refresh.
 
     context "when unliking a post on the posts#show page" do
       it "decrements the likes counter and displays like button live using Turbo Streams" do
@@ -82,6 +70,33 @@ RSpec.describe 'Unlike Post', type: :system, js: true do
         end
 
         expect(page).to have_current_path(post_path(post))
+        expect(page).to have_button('Like')
+        expect(page).to have_selector("turbo-frame#post_#{post.id}_likes")
+        expect(page).to have_content('0 Likes')
+      end
+    end
+  end
+
+  describe "users#show page" do
+    # This test checks the functionality of unliking posts on the posts#show page using Turbo Streams. 
+
+    context "when unliking a post on the posts#show page" do
+      it "decrements the likes counter and displays like button live using Turbo Streams" do
+        post = create(:post, user: user)
+        user.like(post)
+        visit user_path(post.user)
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+
+        expect(page).to have_button('Unlike')
+        expect(page).to have_content('1 Like')
+
+        private_likes_frame = find("turbo-frame#post_#{post.id}\\ private_likes")
+        within(private_likes_frame) do
+          click_button 'Unlike'
+        end
+
+        expect(page).to have_current_path(user_path(post.user))
         expect(page).to have_button('Like')
         expect(page).to have_selector("turbo-frame#post_#{post.id}_likes")
         expect(page).to have_content('0 Likes')

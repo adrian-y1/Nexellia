484ac1ff7b9c028495e1a0451a8e9318c1ee6b58
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-24 19:12:22 +1000

    Added real-time notifications for comment likes

--- app/models/comment_like.rb
+++ app/models/comment_like.rb
@@ -13,4 +13,21 @@ class CommentLike < ApplicationRecord
   belongs_to :liked_comment, class_name: "Comment", foreign_key: "liked_comment_id", counter_cache: true
 
   validates :liker, uniqueness: { scope: :liked_comment_id, message: "has already liked this comment" }
+
+  after_create_commit :create_notification
+  before_destroy :destroy_notifications
+
+  private
+
+  def create_notification
+    return if liker == liked_comment.user
+
+    CommentLikeNotification.with(message: self).deliver_later(liked_comment.user)
+  end
+
+  # Destroys all notifications associated with this like
+  def destroy_notifications
+    notifications = Notification.where(recipient_id: self.liked_comment.user, type: "CommentLikeNotification", params: { message: self })
+    notifications.destroy_all
+  end
 end

29cfda945ed54825a27276d04b5fa78d9fd6d6f4
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-25 19:56:18 +1000

    Added system tests for accepting a friend request notification in real-time using Turbo Streams.
    This commit creates system tests for accepting a friend request notification that is implemented in real-time
    using Turbo Streams.
    
    As both the sender and the receiver of the friend request will receive the notification, Capybara's #using_session helper
    method is used to simulate both users logging in simultaneously in two different sessions. This ensures that they
    both receive the notification in real-time.

---
+++ spec/system/notifications/accepting_friend_request_notification_spec.rb
@@ -0,0 +1,109 @@
+# This file contains system tests for real-time notifications for accepting a friend request using Turbo Streams. 
+#
+# These tests are created using Rspec + Capybara + Capybara's selenium drive
+#
+# To run the tests, run => rspec spec/system/notifications/accepting_friend_request_notification_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe "Accepting Friend Request Notification", type: :system, js: true do
+  let(:sender) { create(:user) }
+  let(:receiver) { create(:user) }
+
+  before do
+    driven_by(:selenium_chrome_headless)
+  end
+
+  # These tests are for sending a notification to both users in real-time when the
+  # friend request receiver accepts the friend request. They also check if the notification is prepended to the correct 
+  # element (the dropdown notifications list or the unread notification in the flash section)
+  #
+  # With the help of Capybara's #using_session helper, we can login both the sender and receiver simultaneously
+  # in two different sessions. This will help determine that they both receive the notification in real-time without
+  # reloading the page and whatnot.
+  #
+  # First, login the receiver in a session and ensure that the subscription to the stream has been 
+  # connected before the broadcast gets called to avoid flaky tests. Same goes for the sender.
+  #
+  # Then, inside the receiver's session, accept the friend request and find the Turbo Frame tag associated with the receiver.
+  # The expectations for the notification and the current path, ensures that the receiver is receiving 
+  # the notification in real-time, without a page refresh/reload confirming Turbo Streams is working.
+  #
+  # Similar process is done for the sender.
+
+  describe "Notifications Dropdown List" do
+    context "when a user accepts a friend request" do
+      it "sends a real-time notification to both users and prepends it to their dropdown notifications list" do
+        create(:friend_request, sender: sender, receiver: receiver)
+
+        using_session :receiver do 
+          login_as(receiver)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
+        
+        using_session :sender do
+          login_as(sender)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
+        
+        using_session :receiver do
+          create(:friendship, user: receiver, friend: sender)
+          find(:id, 'notificationsDropdown').click
+
+          receiver_all_notifications_frame = find("turbo-frame#all_notifications_#{receiver.id}")
+  
+          expect(receiver_all_notifications_frame).to have_content("You and\n#{sender.username}\nare now friends")
+          expect(page).to have_current_path(posts_path)
+        end
+        
+        using_session :sender do
+          find(:id, 'notificationsDropdown').click
+
+          sender_all_notifications_frame = find("turbo-frame#all_notifications_#{sender.id}")
+
+          expect(sender_all_notifications_frame).to have_content("You and\n#{receiver.username}\nare now friends")
+          expect(page).to have_current_path(posts_path)
+        end
+      end
+    end
+  end  
+
+  describe "Unread Notification" do
+    context "when a user accepts a friend request" do
+      it "sends a real-time notification to both users and displays it at the top of their pages" do
+        create(:friend_request, sender: sender, receiver: receiver)
+
+        using_session :receiver do 
+          login_as(receiver)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
+        
+        using_session :sender do
+          login_as(sender)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
+        
+        using_session :receiver do
+          create(:friendship, user: receiver, friend: sender)
+          notifications_frame = find("turbo-frame#notifications")
+          within(notifications_frame) do
+            receiver_unread_notifications_frame = find("turbo-frame#unread_notifications_#{receiver.id}")
+            expect(receiver_unread_notifications_frame).to have_content("You and #{sender.username} are now friends")
+          end
+        end
+        
+        using_session :sender do
+          notifications_frame = find("turbo-frame#notifications")
+          within(notifications_frame) do
+            sender_unread_notifications_frame = find("turbo-frame#unread_notifications_#{sender.id}")
+            expect(sender_unread_notifications_frame).to have_content("You and #{receiver.username} are now friends")
+          end
+        end
+      end
+    end
+  end  
+end

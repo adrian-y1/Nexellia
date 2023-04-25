b96c93c90922cc7074885d30b6624c2aa7bab7d2
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-25 15:52:46 +1000

    Added model tests for the Friendship model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -15,5 +15,37 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:friend) { create(:user) }
+
+  describe "Validation" do
+    context "when the two users are not friends" do
+      it "successfully creates the two-way friendship and friendship is valid" do
+        friendship = create(:friendship, user: user, friend: friend)
+        expect(friendship).to be_valid
+      end
+    end
+
+    context "when the two users are already friends" do
+      it "doesn't create the two-way friendship and is invalid" do
+        friendship = create(:friendship, user: user, friend: friend)
+        new_friendship = build(:friendship, user: user, friend: friend)
+        expect(new_friendship).not_to be_valid
+      end
+    end
+  end
+
+  describe "Association" do
+    describe "User" do
+      let(:friendship) { create(:friendship, user: user, friend: friend) }
+
+      it "belongs to a user(user)" do
+        expect(friendship.user).to eq(user)
+      end
+
+      it "belongs to a user(friend)" do
+        expect(friendship.friend).to eq(friend)
+      end
+    end
+  end
 end

a1765d9341de973896d169fc3ee1012424e6549c
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-29 15:36:32 +1000

    Added system tests for the real-time notification update counter when a notification is marked as read

--- spec/system/notifications/notifications_count_spec.rb
+++ spec/system/notifications/notifications_count_spec.rb
@@ -1,4 +1,4 @@
-# This file contains system tests for real-time unread notifications count using Turbo Streams. 
+# This file contains system tests for real-time notifications count using Turbo Streams. 
 #
 # These tests are created using Rspec + Capybara + Capybara's selenium drive
 #
@@ -15,122 +15,290 @@ RSpec.describe "Notifications Count", type: :system, js: true do
     driven_by(:selenium_chrome_headless)
   end
 
-  # These tests are for updating the unread notifications counter for the recipient in real-time using Turbo Streams
+  # These tests are for updating the unread and read notifications counter for the recipient in real-time using Turbo Streams
   #
+  # ----- UNREAD -----
   # First, ensure that the subscription to the stream has been connected before the broadcast gets called to avoid flaky tests
   # Then, create a database record for the respective notification type for the test examples.
   #
-  # Lastly, the expectations for the notification count starting at '0' then incrementing to '1' after a notification is sent
-  # and the current path, ensures that the recipient is receiving the updated notifications count in real-time, without a page refresh/reload.
+  # Lastly, the expectations for the notification count incrementing and the current path, ensures that the recipient is 
+  # receiving the updated notifications count in real-time, without a page refresh/reload.
+  # Meaning Turbo Streams is working.
+  #
+  # ----- READ -----
+  # First, ensure that the subscription to the stream has been connected before the broadcast gets called to avoid flaky tests
+  # Then, create a database record for the respective notification type for the test examples.
+  #
+  # Then, find the notification url for the respective notification type and open the notifications dropdown.
+  # Find the user's all_notifications Turbo Frame and click on the notification link found earlier.
+  #
+  # Lastly, the expectations for the notification count decrementing and the current path, ensures that the recipient is 
+  # receiving the updated notifications count in real-time, without a page refresh/reload.
   # Meaning Turbo Streams is working.
 
   describe "Friend Request Notification" do
-    it "increments the recipient's unread notifications count in real-time" do
-      visit posts_path
+    context "when the recipient receives a notification" do
+      it "increments the recipient's unread notifications count in real-time" do
+        visit posts_path
+  
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+        expect(notifications_count_frame).to have_content('0')
+  
+        create(:friend_request, sender: user, receiver: recipient)
+  
+        expect(notifications_count_frame).to have_content('1')
+        expect(page).to have_current_path(posts_path)
+      end
+    end
 
-      expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+    context "when the recipient reads a notification by clicking on it" do
+      it "decrements the recipient's unread notifications count in real-time" do
+        visit posts_path
+  
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+        
+        create(:friend_request, sender: user, receiver: recipient)
+        expect(notifications_count_frame).to have_content('1')
 
-      find(:id, 'notificationsDropdown').click
-      notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
-      expect(notifications_count_frame).to have_content('0')
+        sender_notification_url = "/users/#{user.id}?notification_id=#{recipient.notifications.last.id}"
 
-      create(:friend_request, sender: user, receiver: recipient)
+        find(:id, 'notificationsDropdown').click
+        all_notifications_frame = find("turbo-frame#all_notifications_#{recipient.id}")
+        within all_notifications_frame do
+          find("a[href=\"#{sender_notification_url}\"]").click
+        end
 
-      expect(notifications_count_frame).to have_content('1')
-      expect(page).to have_current_path(posts_path)
+        expect(notifications_count_frame).to have_content('0')
+        expect(page).to have_current_path(sender_notification_url)
+      end
     end
   end
 
   describe "New Comment Notification" do
-    it "increments the recipient's unread notifications count in real-time" do
-      post = create(:post, user: recipient)
-      visit posts_path
-      
-      expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-      
-      find(:id, 'notificationsDropdown').click
-      notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
-      expect(notifications_count_frame).to have_content('0')
+    context "when the recipient receives a notification" do
+      it "increments the recipient's unread notifications count in real-time" do
+        post = create(:post, user: recipient)
+        visit posts_path
+        
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        
+        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+        expect(notifications_count_frame).to have_content('0')
+  
+        create(:comment, user: user, post: post)
+        
+        expect(notifications_count_frame).to have_content('1')
+        expect(page).to have_current_path(posts_path)
+      end
+    end
 
-      create(:comment, user: user, post: post)
-      
-      expect(notifications_count_frame).to have_content('1')
-      expect(page).to have_current_path(posts_path)
+    context "when the recipient reads a notification by clicking on it" do
+      it "decrements the recipient's unread notifications count in real-time" do
+        post = create(:post, user: recipient)
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+        
+        create(:comment, user: user, post: post)
+        expect(notifications_count_frame).to have_content('1')
+
+        post_notification_url = "/posts/#{post.id}?notification_id=#{recipient.notifications.last.id}"
+
+        find(:id, 'notificationsDropdown').click
+        all_notifications_frame = find("turbo-frame#all_notifications_#{recipient.id}")
+        within all_notifications_frame do
+          find("a[href=\"#{post_notification_url}\"]").click
+        end
+
+        expect(notifications_count_frame).to have_content('0')
+        expect(page).to have_current_path(post_notification_url)
+      end
     end
   end
 
   describe "Like Notification" do
     describe "Post" do
-      it "increments the recipient's unread notifications count in real-time" do
-        post = create(:post, user: recipient)
-        visit user_path(recipient)
+      context "when the recipient receives a notification" do
+        it "increments the recipient's unread notifications count in real-time" do
+          post = create(:post, user: recipient)
+          visit user_path(recipient)
+  
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+          
+          notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+          expect(notifications_count_frame).to have_content('0')
+          
+          create(:like, likeable: recipient.posts.last, user: user)
+          
+          expect(notifications_count_frame).to have_content('1')
+          expect(page).to have_current_path(user_path(recipient))
+        end
+      end
 
-        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-        
-        find(:id, 'notificationsDropdown').click
-        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
-        expect(notifications_count_frame).to have_content('0')
-        
-        create(:like, likeable: recipient.posts.last, user: user)
-        
-        expect(notifications_count_frame).to have_content('1')
-        expect(page).to have_current_path(user_path(recipient))
+      context "when the recipient reads a notification by clicking on it" do
+        it "decrements the recipient's unread notifications count in real-time" do
+          post = create(:post, user: recipient)
+          visit user_path(recipient)
+  
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+    
+          notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+          
+          create(:like, likeable: recipient.posts.last, user: user)
+          expect(notifications_count_frame).to have_content('1')
+  
+          post_notification_url = "/posts/#{post.id}?notification_id=#{recipient.notifications.last.id}"
+  
+          find(:id, 'notificationsDropdown').click
+          all_notifications_frame = find("turbo-frame#all_notifications_#{recipient.id}")
+          within all_notifications_frame do
+            find("a[href=\"#{post_notification_url}\"]").click
+          end
+  
+          expect(notifications_count_frame).to have_content('0')
+          expect(page).to have_current_path(post_notification_url)
+        end
       end
     end
 
     describe "Comment" do
-      it "increments the recipient's unread notifications count in real-time" do
-        post = create(:post)
-        comment = create(:comment, user: recipient, post: post)
-        visit user_path(recipient)
+      context "when the recipient receives a notification" do
+        it "increments the recipient's unread notifications count in real-time" do
+          post = create(:post)
+          comment = create(:comment, user: recipient, post: post)
+          visit user_path(recipient)
+  
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+          notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+          expect(notifications_count_frame).to have_content('0')
+          
+          create(:like, likeable: recipient.comments.last, user: user)
+          
+          expect(notifications_count_frame).to have_content('1')
+          expect(page).to have_current_path(user_path(recipient))
+        end
+      end
 
-        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-
-        find(:id, 'notificationsDropdown').click
-        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
-        expect(notifications_count_frame).to have_content('0')
-        
-        create(:like, likeable: recipient.comments.last, user: user)
-        
-        expect(notifications_count_frame).to have_content('1')
-        expect(page).to have_current_path(user_path(recipient))
+      context "when the recipient reads a notification by clicking on it" do
+        it "decrements the recipient's unread notifications count in real-time" do
+          post = create(:post)
+          comment = create(:comment, user: recipient, post: post)
+          visit user_path(recipient)
+  
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+    
+          notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+          
+          create(:like, likeable: recipient.comments.last, user: user)
+          expect(notifications_count_frame).to have_content('1')
+  
+          post_notification_url = "/posts/#{post.id}?notification_id=#{recipient.notifications.last.id}"
+  
+          find(:id, 'notificationsDropdown').click
+          all_notifications_frame = find("turbo-frame#all_notifications_#{recipient.id}")
+          within all_notifications_frame do
+            find("a[href=\"#{post_notification_url}\"]").click
+          end
+  
+          expect(notifications_count_frame).to have_content('0')
+          expect(page).to have_current_path(post_notification_url)
+        end
       end
     end
   end
 
   describe "Accepting Friend Request Notification" do
-    it "increments the both the recipient's and the user's unread notifications count in real-time" do
-      create(:friend_request, sender: user, receiver: recipient)
+    context "when both users receive a notification" do
+      it "increments the both the recipient's and the user's unread notifications count in real-time" do
+        create(:friend_request, sender: user, receiver: recipient)
+  
+        using_session :recipient do 
+          login_as(recipient)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
+        
+        using_session :user do
+          login_as(user)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
+        
+        using_session :recipient do
+          notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+          expect(notifications_count_frame).to have_content('1')
+          
+          create(:friendship, user: recipient, friend: user)
+          
+          expect(notifications_count_frame).to have_content('2')
+          expect(page).to have_current_path(posts_path)
+        end
+        
+        using_session :user do
+          notifications_count_frame = find("turbo-frame#user_#{user.id}_notifications_count")
+          
+          expect(notifications_count_frame).to have_content('1')
+          expect(page).to have_current_path(posts_path)
+        end
+      end
+    end
 
-      using_session :recipient do 
-        login_as(recipient)
-        visit posts_path
-        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-      end
-      
-      using_session :user do
-        login_as(user)
-        visit posts_path
-        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-      end
-      
-      using_session :recipient do
-        find(:id, 'notificationsDropdown').click
-        notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
-        expect(notifications_count_frame).to have_content('0')
+    context "when the recipient reads a notification by clicking on it" do
+      it "decrements the recipient's unread notifications count in real-time" do
+        create(:friend_request, sender: user, receiver: recipient)
+  
+        using_session :recipient do 
+          login_as(recipient)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
         
-        create(:friendship, user: recipient, friend: user)
+        using_session :user do
+          login_as(user)
+          visit posts_path
+          expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        end
         
-        expect(notifications_count_frame).to have_content('1')
-        expect(page).to have_current_path(posts_path)
-      end
-      
-      using_session :user do
-        find(:id, 'notificationsDropdown').click
-        notifications_count_frame = find("turbo-frame#user_#{user.id}_notifications_count")
+        using_session :recipient do
+          find(:id, 'notificationsDropdown').click
+          notifications_count_frame = find("turbo-frame#user_#{recipient.id}_notifications_count")
+
+          expect(notifications_count_frame).to have_content('1')
+          create(:friendship, user: recipient, friend: user)
+          expect(notifications_count_frame).to have_content('2')
+
+          friend_notifications_url = "/users/#{user.id}?notification_id=#{recipient.notifications.last.id}"
+          all_notifications_frame = find("turbo-frame#all_notifications_#{recipient.id}")
+          within all_notifications_frame do
+            find("a[href=\"#{friend_notifications_url}\"]").click
+          end
+
+          expect(notifications_count_frame).to have_content('1')
+          expect(page).to have_current_path(friend_notifications_url)
+        end
         
-        expect(notifications_count_frame).to have_content('1')
-        expect(page).to have_current_path(posts_path)
+        using_session :user do
+          find(:id, 'notificationsDropdown').click
+          notifications_count_frame = find("turbo-frame#user_#{user.id}_notifications_count")
+
+          expect(notifications_count_frame).to have_content('1')
+
+          friend_notifications_url = "/users/#{recipient.id}?notification_id=#{user.notifications.last.id}"
+          all_notifications_frame = find("turbo-frame#all_notifications_#{user.id}")
+          within all_notifications_frame do
+            find("a[href=\"#{friend_notifications_url}\"]").click
+          end
+
+          expect(notifications_count_frame).to have_content('0')
+          expect(page).to have_current_path(friend_notifications_url)
+        end
       end
     end
   end

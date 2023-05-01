9a04b93b170dcb8abbb9946f40139479d911de7a
adrian-y1 <adrian.yonan1@gmail.com>
2023-05-01 11:04:07 +1000

    Readded the current picture in the update profile modal form and fixed the ActiveStorage::FileNotFoundError associated with it

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,10 +1,18 @@
 module ProfilesHelper
   def profile_picture_for(profile, size=50)
     if profile.picture.attached? && profile.picture.representable?
-      profile.picture.representation(resize_to_limit: [100, 100]).processed
+      begin
+        profile.picture.representation(resize_to_limit: [100, 100]).processed
+      rescue ActiveStorage::FileNotFoundError
+        gravatar_url_for(profile.user.email, size)
+      end
     else
-      hash = Digest::MD5.hexdigest(profile.user.email.downcase)
-      "https://secure.gravatar.com/avatar/#{hash}?s=#{size}&d=robohash"
+      gravatar_url_for(profile.user.email, size)
     end
+  end
+
+  def gravatar_url_for(email, size)
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://secure.gravatar.com/avatar/#{hash}?s=#{size}&d=robohash"
   end
 end

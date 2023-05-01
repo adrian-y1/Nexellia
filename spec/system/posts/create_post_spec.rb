ee0868a6c2f33fea7fe286cb93bcd628750fbb5c
adrian-y1 <adrian.yonan1@gmail.com>
2023-05-01 23:35:00 +1000

    Added system tests for creating a post with image upload functionality

--- spec/system/posts/create_post_spec.rb
+++ spec/system/posts/create_post_spec.rb
@@ -16,61 +16,189 @@ RSpec.describe "Create Post", type: :system, js: true do
   end
 
   describe "Valid Attributes" do
-    # This test checks that a post can be successfully created using the create post form on the
-    # index page of the application. Since the form is within a Turbo Frame tag, i
-    # do not need to visit a separate page to access it.
-    # 
-    # I use Capybara to fill in the form and submit it. Since i'm using Turbo Streams
-    # instead of a full page refresh, the post is created live and the user is not
-    # redirected to a new page.
-    # 
-    # The test checks that the post is successfully created and that a flash notice is
-    # displayed to the user. I also confirm that Turbo Streams is working by checking
-    # that the post is created live without a page refresh.
+    # Tests that a post can be successfully created using the create post form on the index page.
+    # Uses Capybara to fill in the form and submit it. The post is created live with Turbo Streams,
+    # and the user is not redirected to a new page. Confirms that the post is created, a flash notice
+    # is displayed, and Turbo Streams is working.
 
-    it "creates the post live with Turbo Streams" do
-      visit posts_path
-
-      expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-
-      new_post_frame = find("turbo-frame[id='new_post']")
-      within(new_post_frame) do
-        fill_in 'post[body]', with: content
-        click_on 'Create Post'
+    context "when the post body is valid" do
+      it "creates the post live with Turbo Streams" do
+        visit posts_path
+  
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          click_on 'Create Post'
+        end
+  
+        # Confirms that the post was created live using Turbo Streams and that 
+        # the user is still on the index page.
+        expect(page).to have_current_path(posts_path)
+        expect(page).to have_content(content)
+        expect(page).to have_content(flash_notice)
       end
+    end
 
-      # Confirms that the post was created live using Turbo Streams and that 
-      # the user is still on the index page.
-      expect(page).to have_current_path(posts_path)
-      expect(page).to have_content(content)
-      expect(page).to have_content(flash_notice)
+    context "when the image is of type PNG" do
+      it "creates the post live with Turbo Streams" do
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          attach_file('post[image]', 'spec/fixtures/files/avatar2.png')
+          click_on 'Create Post'
+        end
+
+        expect(page).to have_content(content)
+        expect(page).to have_css('img[src$="avatar2.png"]')
+        expect(page).to have_content(flash_notice)
+        expect(page).to have_current_path(posts_path)
+      end
+    end
+
+    context "when the image is of type JPG" do
+      it "creates the post live with Turbo Streams" do
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          attach_file('post[image]', 'spec/fixtures/files/testing_image.jpg')
+          click_on 'Create Post'
+        end
+
+        expect(page).to have_content(content)
+        expect(page).to have_css('img[src$="testing_image.jpg"]')
+        expect(page).to have_content(flash_notice)
+        expect(page).to have_current_path(posts_path)
+      end
+    end
+
+    context "when the image is of type JPEG" do
+      it "creates the post live with Turbo Streams" do
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          attach_file('post[image]', 'spec/fixtures/files/testing_image.jpeg')
+          click_on 'Create Post'
+        end
+
+        expect(page).to have_content(content)
+        expect(page).to have_css('img[src$="testing_image.jpeg"]')
+        expect(page).to have_content(flash_notice)
+        expect(page).to have_current_path(posts_path)
+      end
+    end
+
+    context "when the image is of type GIF" do
+      it "creates the post live with Turbo Streams" do
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          attach_file('post[image]', 'spec/fixtures/files/test.gif')
+          click_on 'Create Post'
+        end
+
+        expect(page).to have_content(content)
+        expect(page).to have_css('img[src$="test.gif"]')
+        expect(page).to have_content(flash_notice)
+        expect(page).to have_current_path(posts_path)
+      end
+    end
+
+    context "when post body is provided but image is not provided" do
+      it "creates the post live with Turbo Streams" do
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          click_on 'Create Post'
+        end
+
+        expect(page).to have_content(content)
+        expect(page).to have_content(flash_notice)
+        expect(page).to have_current_path(posts_path)
+      end
     end
   end
 
   describe "Invalid Attributes" do
-    # This test checks that a post cannot be created with empty body field 
-    # using the create post form on the index page of the application. 
-    # Since the form is within a Turbo Frame tag, i do not need to visit a separate page to access it.
-    # 
-    # I use Capybara to fill in the form and submit it. Since i'm using Turbo Streams
-    # instead of a full page refresh, the flash notice appears with no page reload.
+    # Tests that a post cannot be successfully created using the create post form on the index page when 
+    # it has invalid attributes.
+    # Uses Capybara to fill in the form and submit it. The post is created live with Turbo Streams,
+    # and the user is not redirected to a new page. Confirms that the post is created, a flash notice
+    # is displayed, and Turbo Streams is working.
     
-    it "doesn't create the post and renders an error without page referesh" do
-      visit posts_path
+    context "when the post body is empty" do
+      it "doesn't create the post and renders an error without page referesh" do
+        visit posts_path
+  
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: ''
+          click_on 'Create Post'
+        end
 
-      expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
-
-      new_post_frame = find("turbo-frame[id='new_post']")
-      within(new_post_frame) do
-        fill_in 'post[body]', with: ''
-        click_on 'Create Post'
+        expect(page).to have_current_path(posts_path)
+        expect(page).to have_content("Body can't be blank")
       end
-      
-      # Since the user hasn't been redirected after creating a post,
-      # this confirms they are still on the index page and the flash notice
-      # is displayed as a result of using Turbo Streams 
-      expect(page).to have_current_path(posts_path)
-      expect(page).to have_content("Body can't be blank")
+    end
+
+    context "when the image is not of type PNG, JPG, JPEG or GIF" do
+      it "doesn't create the post and renders an error without page referesh" do
+        visit posts_path
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: content
+          attach_file('post[image]', 'spec/fixtures/files/text.txt')
+          click_on 'Create Post'
+        end
+
+        expect(page).to_not have_css('img[src$="text.txt"]')
+        expect(page).to have_content('Image needs to be a JPEG, JPG, PNG or GIF')
+        expect(page).to have_current_path(posts_path)
+      end
+    end
+
+    context "when post body is not provided but image is provided" do
+      it "doesn't create the post and renders an error without page referesh" do
+        visit posts_path
+  
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+  
+        new_post_frame = find("turbo-frame[id='new_post']")
+        within(new_post_frame) do
+          fill_in 'post[body]', with: ''
+          attach_file('post[image]', 'spec/fixtures/files/avatar2.png')
+          click_on 'Create Post'
+        end
+
+        expect(page).to have_current_path(posts_path)
+        expect(page).to have_content("Body can't be blank")
+      end
     end
   end
 end

c6fad26cbc9bbd5c90bea4a3ab433ef7393431e8
adrian-y1 <adrian.yonan1@gmail.com>
2023-05-05 12:45:38 +1000

    Added system tests for creating a new reply comment notification

--- spec/system/notifications/new_comment_notification_spec.rb
+++ spec/system/notifications/new_comment_notification_spec.rb
@@ -44,6 +44,25 @@ RSpec.describe "New Comment Notification", type: :system, js: true do
         expect(page).to have_current_path(posts_path)
       end
     end
+
+    context "when a user replies to a comment" do
+      it "sends a real-time notification to the parent comment creator and prepends it to their dropdown notifications list" do
+        post = create(:post, user: author)
+        visit posts_path
+        
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+        
+        comment = create(:comment, user: author, commentable: post)
+        create(:comment, commentable: comment, parent: comment, user: commenter)
+
+        find(:id, 'notificationsDropdown').click
+
+        dropdown_notifications_frame = find("turbo-frame#dropdown_notifications_#{author.id}")
+
+        expect(dropdown_notifications_frame).to have_content("#{commenter.username} replied to your comment")
+        expect(page).to have_current_path(posts_path)
+      end
+    end
   end  
 
   describe "New Notifications" do
@@ -60,6 +79,26 @@ RSpec.describe "New Comment Notification", type: :system, js: true do
         within(notifications_frame) do
           new_notifications_frame = find("turbo-frame#new_notifications_#{author.id}")
           expect(new_notifications_frame).to have_content("#{commenter.username} commented on your post")
+        end
+
+        expect(page).to have_current_path(user_path(author))
+      end
+    end
+
+    context "when a user replies to a comment" do
+      it "sends a real-time notification to the parent comment author and prepends it to their new_notifications frame under navbar" do
+        post = create(:post, user: author)
+        visit user_path(author)
+
+        expect(page).to have_css('turbo-cable-stream-source[connected]', visible: false)
+
+        comment = create(:comment, user: author, commentable: post)
+        create(:comment, commentable: comment, user: commenter, parent: comment)
+
+        notifications_frame = find("turbo-frame#notifications")
+        within(notifications_frame) do
+          new_notifications_frame = find("turbo-frame#new_notifications_#{author.id}")
+          expect(new_notifications_frame).to have_content("#{commenter.username} replied to your comment")
         end
 
         expect(page).to have_current_path(user_path(author))

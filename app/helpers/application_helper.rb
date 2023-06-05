f8aed171b20f888e91c75074fa7e08c5a2eb1e7e
adrian-y1 <adrian.yonan1@gmail.com>
2023-06-05 20:45:28 +1000

    Added active link for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
   def nested_dom_id(parent, child)
     "#{dom_id(parent)}_#{dom_id(child)}"
   end
+
+  def set_active_link(path)
+    is_current_path?(path) ? "active-link" : ""
+  end
+
+  def set_fill_icon(path)
+    is_current_path?(path) ? "-fill" : ""
+  end
+
+  def set_active_border(path)
+    is_current_path?(path) ? content_tag(:div, "", class: "active-link-border") : ""
+  end
+
+  def is_current_path?(path)
+    request.path == path
+  end
 end

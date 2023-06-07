258202615d7aef5481560ac3317c5e9d19067ba9
adrian-y1 <adrian.yonan1@gmail.com>
2023-06-07 14:47:17 +1000

    Styled form errors and changed their structure

--- app/javascript/controllers/close_flash_controller.js
+++ app/javascript/controllers/close_flash_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    this.element.addEventListener('animationend', this.handleAnimationEnd.bind(this));
+  }
+
   close() {
     this.element.remove()
+  }
+
+  handleAnimationEnd() {
+    this.element.remove();
   }
 }

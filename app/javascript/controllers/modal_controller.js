4a97cc041b87bfbaf9897cc93fabbd64f20ea8c1
adrian-y1 <adrian.yonan1@gmail.com>
2023-06-25 15:55:02 +1000

    Handle window click outside modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,14 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal"]
 
+  connect() {
+    // Bind the handleOutsideClick function to the controller instance
+    this.handleOutsideClick = this.handleOutsideClick.bind(this); 
+  }
+
   open() {
     this.modalTarget.style.display = "flex"
     document.body.classList.add('modal-open');
+    this.modalTarget.addEventListener("click", this.handleOutsideClick)
   }
 
-  // Closes the modal if the form was successfully submitted
   close() {
     this.modalTarget.style.display = "none"
     document.body.classList.remove('modal-open');
+    this.modalTarget.removeEventListener("click", this.handleOutsideClick)
+  }
+
+  handleOutsideClick(event) {
+    // Check if the clicked element has the "modal" class
+    if (event.target.classList.contains("modal")) {
+      this.close(); 
+    }
   }
 }

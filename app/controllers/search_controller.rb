58cb237b71acb378c8b372f0e80edd3007a7185a
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-05 20:46:43 +1000

    Added a user search in the navbar that goes to search#index and styled it

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,9 @@
 class SearchController < ApplicationController
+  def index
+    if params[:q].present?
+      @users = User.by_first_last_name(params[:q]).load_profiles
+    else
+      redirect_to posts_path
+    end
+  end
 end

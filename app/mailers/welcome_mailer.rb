0e0b38849ec166c64a8bd31f2217397c84ffc159
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-06 21:23:49 +1000

    Changed website name to Nexellia and added new logo

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,6 +4,6 @@ class WelcomeMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = 'http://127.0.0.1:3000/users/sign_in'
-    mail(to: @user.email, subject: 'Welcome to EchoVerse!')
+    mail(to: @user.email, subject: 'Welcome to Nexellia!')
   end
 end

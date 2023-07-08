87a6ce6eb6111acf7d81d40dd3c45123a92c80d9
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-08 14:01:57 +1000

    Renamed the app odin_facebook_logo.png file, styled the reset password instructions page and added the app's logo to the welcome mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,6 +4,7 @@ class WelcomeMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = 'http://127.0.0.1:3000/users/sign_in'
+    attachments.inline['nexellia.png'] = File.read('app/assets/images/nexellia.png')
     mail(to: @user.email, subject: 'Welcome to Nexellia!')
   end
 end

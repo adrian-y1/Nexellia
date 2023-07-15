a3e53fbface22b4f0770779d65098ef4a8aa1c20
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-15 19:32:34 +1000

    Fix js not compiling in production issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,4 +107,8 @@ Rails.application.configure do
     enable_starttls_auto: true,
     open_timeout:         5,
     read_timeout:         5 }
+
+  # Assets configs
+  config.assets.precompile += %w( *.js *.css )
+  config.serve_static_assets = true
 end

56acfbe1576bdd895f37275a36b4f600bc41ebfb
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-15 16:08:32 +1000

    Changed application.js imports back to original state

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./javascript/components/notifications"
-import "./javascript/components/search"
-import "./javascript/components/search_friends"
+import "./components/notifications"
+import "./components/search"
+import "./components/search_friends"
 

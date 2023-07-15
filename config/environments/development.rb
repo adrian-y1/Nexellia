f35c4d4e01f9992b7f5d9b83ae8845daf442b99e
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-15 14:48:00 +1000

    SMTP Email Configuration for development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,22 @@ Rails.application.configure do
   config.action_controller.default_url_options = { host: 'localhost', port: 3000 }
 
   # letter_opener gem config
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :file
+
+  # SMTP Configuration
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_APP_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
 end

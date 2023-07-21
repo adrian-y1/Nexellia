6dfae50147f938960bc5670712a28aa13978e47f
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-21 13:17:57 +1000

    Remove SMTP from development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,20 +83,5 @@ Rails.application.configure do
   # letter_opener gem config
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :file
-
-  # SMTP Configuration
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'nexellia.onrender.com',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_APP_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 }
-
+  config.action_mailer.delivery_method = :file
 end

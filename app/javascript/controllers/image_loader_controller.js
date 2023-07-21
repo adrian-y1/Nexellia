b6e0356a041ea3b798048af31bc9544dcbba0e5c
adrian-y1 <adrian.yonan1@gmail.com>
2023-07-21 12:41:44 +1000

    Fix images in posts not loading sometimes due to race condition with turbo

---
+++ app/javascript/controllers/image_loader_controller.js
@@ -0,0 +1,52 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+
+  static targets = [ "image" ]
+
+  connect() {
+    this.imageTarget.addEventListener("error", this.handleError)  
+  }
+  
+  handleError = () => {
+    let placeholder_image = new Image()
+    placeholder_image.src = "assets/placeholder_image.jpg" 
+
+    // Copy attributes from original image to the placeholder image
+    placeholder_image = this.copyAttributes(placeholder_image, this.imageTarget)
+
+    // Replace original with placeholder_image
+    this.imageTarget.parentNode.replaceChild(placeholder_image, this.imageTarget)
+    
+    let retries = 0
+    const reloadImage = () => {
+      retries++
+      if(retries < 3) {
+        setTimeout(() => {
+          this.imageTarget.src = this.imageTarget.src
+          reloadImage() 
+        }, 2000)
+      }
+    }
+
+    this.replaceImage(placeholder_image)
+    
+    reloadImage()
+  }
+
+  copyAttributes(placeholder_image, originalImage) {
+    placeholder_image.width = originalImage.width
+    placeholder_image.style.maxHeight = "600px"
+    placeholder_image.setAttribute("data-picture-modal-target", "postShowPicture")
+    placeholder_image.setAttribute("data-modal-target", "modal")
+    return placeholder_image
+  }
+
+  replaceImage(placeholder_image) {
+    this.imageTarget.onload = () => {
+      if (placeholder_image.parentNode !== null) {
+        placeholder_image.parentNode.replaceChild(this.imageTarget, placeholder_image)
+      }
+    }
+  }
+}
